6591d162ae819c018466ff751bdbffb72f882dcf
Roman Sapunov <youngkicker@mail.ru>
2020-11-12 17:13:36 +0400

    create project in self_task\work 11.11.2020

---
+++ self_task/work 11.11.2020/MDK1301_11.11.20_Sapunov/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDK1301_11._11._20_Sapunov
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
