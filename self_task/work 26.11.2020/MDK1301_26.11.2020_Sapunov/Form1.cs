613121a8a380243a140c836aa239e420b4c93404
Roman Sapunov <youngkicker@mail.ru>
2020-11-26 17:25:35 +0400

    add project in self_task\work 26.11.2020

---
+++ self_task/work 26.11.2020/MDK1301_26.11.2020_Sapunov/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDK1301_26._11._2020_Sapunov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
